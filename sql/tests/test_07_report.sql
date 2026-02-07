96c3c377a5cbf5f75eba0e1908199bd763092c95
walaa <wo441170@gmail.com>
2026-02-07 15:47:20 +0200

    Create test_07_report.sql

---
+++ sql/tests/test_07_report.sql
@@ -0,0 +1,12 @@
+-- TASK 7 - Report execution placeholder
+SET SERVEROUTPUT ON;
+
+DECLARE
+  v_dummy NUMBER;
+BEGIN
+  -- If you have a report procedure, call it here.
+  -- Example:
+  -- User1.generate_performance_report;
+  NULL;
+END;
+/
