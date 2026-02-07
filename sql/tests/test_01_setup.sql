d4c058c78a094b01565489c7392649e343c07237
walaa <wo441170@gmail.com>
2026-02-07 15:44:06 +0200

    Create test_01_setup.sql

---
+++ sql/tests/test_01_setup.sql
@@ -0,0 +1,7 @@
+-- TASK 1 - Setup verification
+SELECT
+  (SELECT COUNT(*) FROM dba_users 
+   WHERE username IN ('MANAGERUSER','USER1','USER2')) AS users_created,
+  (SELECT COUNT(*) FROM dba_tablespaces 
+   WHERE tablespace_name = 'HOSPITAL_TBS') AS tbs_created
+FROM dual;
